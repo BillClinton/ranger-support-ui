4d3fbb11fb43cf6c6806347cac56762ba794bbd0
BillClinton <bclinton@appunto.net>
2019-07-13 16:40:14 -0400

    Add a TODO note for inline style removal

--- src/components/clients/ClientItem.js
+++ src/components/clients/ClientItem.js
@@ -7,6 +7,7 @@ const ClientItem = ({ client }) => {
       <td data-label="Name">{client.name}</td>
       <td data-label="Contact">{client.contact}</td>
       <td data-label="Contact Number">{client.contact_number}</td>
+      {/* TODO: put this style in a css class */}
       <td data-label="Actions" style={{ paddingTop: 0, paddingBottom: 0 }}>
         <Link to={`/clients/edit/${client.id}`} className="ui icon button">
           <i aria-hidden="true" className="edit icon" />
