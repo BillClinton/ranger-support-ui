2fd9f5004c37d0fca461d17fad6945e99b97ed2e
BillClinton <bclinton@appunto.net>
2019-07-13 16:35:01 -0400

    Make header fixed, inverted

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,17 +3,19 @@ import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className="ui secondary pointing menu">
-      <Link to="/" className="item">
-        Ranger
-      </Link>
-      <div className="right menu">
-        <NavLink to="/jobs" className="item" activeClassName="active">
-          Jobs
-        </NavLink>
-        <NavLink to="/clients" className="item" activeClassName="active">
-          Clients
-        </NavLink>
+    <div class="ui fixed inverted menu">
+      <div class="ui container">
+        <Link to="/" className="header item">
+          Ranger
+        </Link>
+        <div className="right menu">
+          <NavLink to="/jobs" className="item" activeClassName="active">
+            Jobs
+          </NavLink>
+          <NavLink to="/clients" className="item" activeClassName="active">
+            Clients
+          </NavLink>
+        </div>
       </div>
     </div>
   );
