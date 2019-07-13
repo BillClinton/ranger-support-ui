d1f07b06a82768087cf1273d0b535a8f689daea6
BillClinton <bclinton@appunto.net>
2019-07-12 21:04:13 -0400

    Add form reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
+import { reducer as formReducer } from "redux-form";
 import clientReducer from "./clientReducer";
 
 export default combineReducers({
+  form: formReducer,
   clients: clientReducer
 });
