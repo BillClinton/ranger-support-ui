85272b95c0f799c2106f9f0b85ba50485047569d
BillClinton <bclinton@appunto.net>
2019-07-22 01:42:55 -0400

    Add delete component and switch routes

--- src/components/clients/ClientsHome.js
+++ src/components/clients/ClientsHome.js
@@ -1,20 +1,24 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 import ClientList from "./ClientList";
 import ClientCreate from "./ClientCreate";
 import ClientEdit from "./ClientEdit";
 import ClientView from "./ClientView";
+import ClientDelete from "./ClientDelete";
 import ScrollToTop from "../ScrollToTop";
 
 const ClientsHome = () => {
   return (
     <ScrollToTop>
       <div className="ui container" style={{ paddingTop: "60px" }}>
-        <Route path="/clients" exact component={ClientList} />
-        <Route path="/clients/new/" exact component={ClientCreate} />
-        <Route path="/clients/edit/:id" exact component={ClientEdit} />
-        <Route path="/clients/view/:id" exact component={ClientView} />
+        <Switch>
+          <Route path="/clients" exact component={ClientList} />
+          <Route path="/clients/new/" exact component={ClientCreate} />
+          <Route path="/clients/edit/:id" exact component={ClientEdit} />
+          <Route path="/clients/view/:id" exact component={ClientView} />
+          <Route path="/clients/delete/:id" exact component={ClientDelete} />
+        </Switch>
       </div>
     </ScrollToTop>
   );
